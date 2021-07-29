0c31b9b113c12fd9dff540332682f94c635982cf
Alex Bright <abrighttt@gmail.com>
2021-07-28 21:14:02 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -11,11 +11,16 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
-})
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/July2021',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
